17b87f1160c7acb566bb9825533eb0593b4f0520
Jéssica Gouveia <jehsgouveia@gmail.com>
2021-09-04 14:12:25 -0300

    Adicionando config de port no server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app')
-const port = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`)
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
 })
